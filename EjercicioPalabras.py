2a0ad4286d794a273e4dd913344ced15741d2317
Tomas Rodriguez <franco.tomas.rodriguez@gmail.com>
2026-03-26 08:34:04 -0300

    Ejercicio Palabras

---
+++ EjercicioPalabras.py
@@ -0,0 +1,48 @@
+# Escriba un programa que permita crear una lista de palabras y que, a continuación de tres opciones:
+
+# * Contar: Me pide una cadena, y me dice cuantas veces aparece en la lista
+# * Modificar: Me pide una cadena, y otra cadena a modificar, y modifica todas alas apariciones de la primera por la 
+# segunda en la lista.
+# * Eliminar: Me pide una cadena, y la elimina de la lista.
+# * Mostrar: Muestra la lista de cadenas
+# * Terminar
+
+import os
+os.system("cls")
+
+palabras = []
+
+seguir = True
+
+while seguir:
+    p = input("Ingrese una palabra:")
+    if p == "":
+        seguir = False
+    else:
+        palabras.append(p)
+
+opcion = input("Ingrese una opcion (Contar, Modificar, Eliminar, Mostrar o Terminar):").lower()
+
+if opcion == "terminar":
+    print("Gracias por jugar!")
+    
+if opcion == "contar":
+    print(len(palabras))
+    
+if opcion == "modificar":
+    input_mod = input("Ingrese la palabra a modificar:").lower()
+    input_mod2 = input("Ingrese la palabra la modificacion:").lower()
+    for i in palabras:
+        if i == input_mod:
+            i = input_mod2
+            
+if opcion == "eliminar":
+    input_elim = input("Ingrese la palabra a eliminar:").lower()
+    for i in palabras:
+        if i == input_elim:
+            palabras.remove(i)
+            
+if opcion == "mostrar":
+    print(palabras)
+    
+    
